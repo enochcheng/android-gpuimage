3392f357e82b3031c58de3846cecdda6b2cc4c6d
Jesse Scott <jessecolinscott@gmail.com>
2014-07-15 14:45:08 -0400

    Created GPUImageMaskFilter.java

---
+++ library/src/jp/co/cyberagent/android/gpuimage/GPUImageMaskFilter.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2012 CyberAgent
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Taken From https://github.com/joplaete/android-gpuimage/blob/36c7510bf07ebfaeae75abd607efe29699fe8bb7/library/src/jp/co/cyberagent/android/gpuimage/GPUImageMaskFilter.java
+ */
+package jp.co.cyberagent.android.gpuimage;
+
+public class GPUImageMaskFilter extends GPUImageTwoInputFilter {
+	public static final String MASK_FRAGMENT_SHADER = "" + 
+			" varying highp vec2 textureCoordinate;\n" +
+    			" varying highp vec2 textureCoordinate2;\n" +
+            "\n" +
+            " uniform sampler2D inputImageTexture;\n" +
+            " uniform sampler2D inputImageTexture2;\n" +
+            "\n" +
+            " void main()\n" +
+            " {\n" +
+            "   lowp vec4 textureColor = texture2D(inputImageTexture, textureCoordinate);\n" +
+            "   lowp vec4 textureColor2 = texture2D(inputImageTexture2, textureCoordinate2);\n" +
+            "\n" +
+            "   lowp float newAlpha = dot(textureColor2.rgb, vec3(.33333334, .33333334, .33333334)) * textureColor2.a;" +
+            "\n" +
+            "   gl_FragColor = vec4(textureColor.xyz, newAlpha);" +
+            " }";
+
+    public GPUImageMaskFilter() {
+        super(MASK_FRAGMENT_SHADER);
+    }
+}
