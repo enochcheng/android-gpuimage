a8e2c6cc61c6627579d93bcf03a7b2188edb6bef
Jesse Scott <jessecolinscott@gmail.com>
2014-07-15 20:01:15 -0400

    fixed PolkaDotFilter

--- library/src/jp/co/cyberagent/android/gpuimage/GPUImagePolkaDotFilter.java
+++ library/src/jp/co/cyberagent/android/gpuimage/GPUImagePolkaDotFilter.java
@@ -27,34 +27,35 @@ import android.opengl.GLES20;
  */
 public class GPUImagePolkaDotFilter extends GPUImageFilter {
     public static final String POLKA_DOT_FRAGMENT_SHADER = "" +
-    		"precision highp float;\n" +
+    		" precision highp float;\n" +
     		
-    		"varying highp vec2 textureCoordinate;\n" +
-		"uniform sampler2D inputImageTexture;\n" +
-	
-		"uniform highp float fractionalWidthOfPixel;\n" +
-		"uniform highp float aspectRatio;\n" +
-		"uniform highp float dotScaling;\n" +
+		" varying highp vec2 textureCoordinate;\n" +
+		    		 
+		" uniform sampler2D inputImageTexture;\n" +
+		 
+		" uniform highp float fractionalWidthOfPixel;\n" +
+		" uniform highp float aspectRatio;\n" +
+		" uniform highp float dotScaling;\n" +
 	
 		"void main()\n" +
 			"{\n" +
-			" highp vec2 sampleDivisor = vec2(fractionalWidthOfPixel, fractionalWidthOfPixel / aspectRatio);\n" +
-			
-			" highp vec2 samplePos = textureCoordinate - mod(textureCoordinate, sampleDivisor) + 0.5 * sampleDivisor;\n" +
-			" highp vec2 textureCoordinateToUse = vec2(textureCoordinate.x, (textureCoordinate.y * aspectRatio + 0.5 - 0.5 * aspectRatio));\n" +
-			" highp vec2 adjustedSamplePos = vec2(samplePos.x, (samplePos.y * aspectRatio + 0.5 - 0.5 * aspectRatio));\n" +
-			" highp float distanceFromSamplePoint = distance(adjustedSamplePos, textureCoordinateToUse);\n" +
-			" lowp float checkForPresenceWithinDot = step(distanceFromSamplePoint, (fractionalWidthOfPixel * 0.5) * dotScaling);\n" +
-			
-			" lowp vec4 inputColor = texture2D(inputImageTexture, samplePos);\n" +
-			
-			" gl_FragColor = vec4(inputColor.rgb * checkForPresenceWithinDot, inputColor.a);\n" +
+		     " highp vec2 sampleDivisor = vec2(fractionalWidthOfPixel, fractionalWidthOfPixel / aspectRatio);\n" +
+				     
+		     " highp vec2 samplePos = textureCoordinate - mod(textureCoordinate, sampleDivisor) + 0.5 * sampleDivisor;\n" +
+		     " highp vec2 textureCoordinateToUse = vec2(textureCoordinate.x, (textureCoordinate.y * aspectRatio + 0.5 - 0.5 * aspectRatio));\n" +
+		     " highp vec2 adjustedSamplePos = vec2(samplePos.x, (samplePos.y * aspectRatio + 0.5 - 0.5 * aspectRatio));\n" +
+		     " highp float distanceFromSamplePoint = distance(adjustedSamplePos, textureCoordinateToUse);\n" +
+		     " lowp float checkForPresenceWithinDot = step(distanceFromSamplePoint, (fractionalWidthOfPixel * 0.5) * dotScaling);\n" +
+		
+		     " lowp vec4 inputColor = texture2D(inputImageTexture, samplePos);\n" +
+		     
+		     " gl_FragColor = vec4(inputColor.rgb * checkForPresenceWithinDot, inputColor.a);\n" +
 	    "}";
 
 
-    private int mDotLocation;
-    private int fractionalWidthOfAPixelLocation;
-    private int aspectRatioLocation;
+    private int dotScalingUniform;
+    private int fractionalWidthOfAPixelUniform;
+    private int aspectRatioUniform;
     
     public GPUImagePolkaDotFilter() {
         super(NO_FILTER_VERTEX_SHADER, POLKA_DOT_FRAGMENT_SHADER);
@@ -64,16 +65,26 @@ public class GPUImagePolkaDotFilter extends GPUImageFilter {
     public void onInit() {
         super.onInit();
 
-        fractionalWidthOfAPixelLocation = GLES20.glGetUniformLocation(getProgram(), "fractionalWidthOfPixel");
-        aspectRatioLocation = GLES20.glGetUniformLocation(getProgram(), "aspectRatio");
-        mDotLocation = GLES20.glGetUniformLocation(getProgram(), "dotScaling");
+        dotScalingUniform = GLES20.glGetUniformLocation(getProgram(), "dotScaling");
+        fractionalWidthOfAPixelUniform = GLES20.glGetUniformLocation(getProgram(), "fractionalWidthOfPixel");
+        aspectRatioUniform = GLES20.glGetUniformLocation(getProgram(), "aspectRatio");
         
-        setFloat(fractionalWidthOfAPixelLocation, 0.05f);              
-        setFloat(aspectRatioLocation, 1.0f);
-        setDotScaling(1.0f);
+        setDotScaling(0.1f);
+        setAspectRatio(1.0f);
+        //setFractionalWidthOfAPixel(0.1f);     
     }
 
-    public void setDotScaling(final float dot) {
-    		setFloat(mDotLocation, dot);
+    public void setDotScaling(final float scale) {
+    		//float newScale = (float) (Math.pow(scale, 4) / 6);
+    		setFloat(dotScalingUniform, scale);
     }
+    
+    public void setAspectRatio(final float ratio) {
+    		setFloat(aspectRatioUniform, ratio);
+    }
+
+    public void setFractionalWidthOfAPixel(final float fraction) {
+    		setFloat(fractionalWidthOfAPixelUniform, fraction); 
+    }
+    
 }
